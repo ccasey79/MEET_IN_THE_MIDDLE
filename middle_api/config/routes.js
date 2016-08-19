89068de9e5fe7c2fb00df6bf441551c5069c4b58
Jason Lai <jase_lai@hotmail.com>
2016-08-19 18:30:18 +0100

    user model working with insomnia

--- middle_api/config/routes.js
+++ middle_api/config/routes.js
@@ -1,1 +1,27 @@
 var router = require('express').Router();
+
+var usersController = require('../controllers/users');
+var authController = require("../controllers/authentications");
+
+//require json web token stuff
+var secret = require("../config/tokens").secret;
+var jwt = require("jsonwebtoken");
+
+//middleware to check for token 
+function secureRoute(req, res, next){
+  if(!req.headers.authorization) return res.status(401).json({ message: "Unauthorized!" });
+  var token = req.headers.authorization.replace("Bearer ", "");
+
+  jwt.verify(token, secret, function(err, payload){
+    if(err || !payload) return res.status(401).json({ message: "Unauthorized!" });
+    
+    req.user = payload;
+    next(); 
+  });
+}
+
+router.route('/users')
+  .get(usersController.index)
+
+
+module.exports = router;
--- middle_api/controllers/users.js
+++ middle_api/controllers/users.js
@@ -1,1 +1,12 @@
 var User = require('../models/user');
+
+function usersIndex(req, res) {
+  User.find(function(err, shoes) {
+    if(err) return res.status(500).json(err);
+    return res.status(200).json(shoes);
+  });
+}
+
+module.exports = {
+  index: usersIndex,
+}
