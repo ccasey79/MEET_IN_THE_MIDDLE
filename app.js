945a746da3930f8a0d20f1708cc80c23e09f530a
christian <christiancasey969@gmail.com>
2016-08-25 19:10:42 +0100

    Updated file structure for deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 var routes = require('./config/routes');
 
@@ -22,6 +22,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use('/api', routes);
 
--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/middle_api_test",
   development: "mongodb://localhost/middle_api",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/middle_api"
+  production: process.env.MONGODB_URI || "mongodb://localhost/middle_api"
 }
 
 module.exports = function(env) {
