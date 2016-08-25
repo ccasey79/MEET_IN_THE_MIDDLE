8fa6a0f03196f98a195a053f626c789d4105ca52
Jason Lai <jase_lai@hotmail.com>
2016-08-25 10:53:37 +0100

    distance added to route

--- middle_app/js/gmaps.js
+++ middle_app/js/gmaps.js
@@ -348,19 +348,21 @@ gMaps.findRoute = function(place) {
       $(".totalDuration").remove();
 
       gMaps.directionsDisplay.setDirections(response);
-      var route = response.routes[0].legs[0].steps; 
-      var duration = 0;
+      var route = response.routes[0].legs[0].steps;  
+      var totalDistance = response.routes[0].legs[0].distance.text
+      var totalDuration = response.routes[0].legs[0].duration.text
+
+      console.log(totalDistance+" "+totalDuration);
+
       for (i = 0; i < route.length; i++) { 
-          console.log(route[i]);
 
           $("#routeSteps").append("<div class='routeStep'>"+route[i].instructions+"</div>" + 
-            "<div class='duration'>"+ route[i].duration.text +"<hr></div>");
-
-        duration += route[i].duration.value;
+            "<div class='duration'>" + route[i].distance.text + ", " + route[i].duration.text  + "<hr></div>");
       }
-      duration = Math.round(duration/60);
 
-      $("#routeSteps").append("<div class='totalDuration'>Total estimated time: "+ duration +" minutes.<hr></div>");
+      $("#routeSteps").append("<div class='totalDuration'>Journey Total: "+ 
+        totalDistance + ", "+
+        totalDuration + "<hr></div>");
     }  
   });
 }
